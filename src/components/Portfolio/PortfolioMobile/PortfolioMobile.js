3300b4b58e31297bc2bb003aa636ef138011b662
poterstar <poter.t1mox@gmail.com>
2017-07-26 14:27:31 +0300

    added portdolio slider for mobile version

--- src/components/Portfolio/PortfolioMobile/PortfolioMobile.js
+++ src/components/Portfolio/PortfolioMobile/PortfolioMobile.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import first from '../../../img/portfolio/1.png';
 import second from '../../../img/portfolio/2.png';
@@ -10,20 +13,63 @@ import eighth from '../../../img/portfolio/8.png';
 import ninth from '../../../img/portfolio/9.png';
 import tenth from '../../../img/portfolio/10.png';
 
-const PortfolioMobile = () => {
-	return (
-		<div className="portfolio-mobile">
-			<img src={first} className="first" alt=""/>
-			<img src={second} className="second" alt=""/>
-			<img src={third} className="third" alt=""/>
-			<img src={fourth} className="fourth" alt=""/>
-			<img src={tenth} className="tenth" alt=""/>
-			<img src={seventh} className="seventh" alt=""/>
-			<img src={eighth} className="eighth" alt=""/>
-			<img src={ninth} className="ninth" alt=""/>
-			<img src={sixth} className="sixth" alt=""/>
-		</div>
-	)
-};
+class PortfolioMobile extends Component {
+	constructor(props, context) {
+		super(props, context);
+		AOS.init();
+	}
+	componentWillReceiveProps() {
+		AOS.refresh();
+	}
+
+	render() {
+		const settingsMobile = {
+			initialSlide: 1,
+			slidesToShow: 1,
+			infinite: true,
+			arrows: false,
+			fade: false,
+			draggable: true,
+			dots: true
+		};
+		return (
+			<Slider {...settingsMobile} >
+				<div className="portfolio-mobile">
+					<img src={first} className="first" alt=""/>
+					<img src={second} className="second" alt=""/>
+					<img src={third} className="third" alt=""/>
+					<img src={fourth} className="fourth" alt=""/>
+					<img src={tenth} className="tenth" alt=""/>
+					<img src={seventh} className="seventh" alt=""/>
+					<img src={eighth} className="eighth" alt=""/>
+					<img src={ninth} className="ninth" alt=""/>
+					<img src={sixth} className="sixth" alt=""/>
+				</div>
+				<div className="portfolio-mobile" >
+					<img src={first} className="first" alt=""/>
+					<img src={second} className="second" alt=""/>
+					<img src={third} className="third" alt=""/>
+					<img src={fourth} className="fourth" alt=""/>
+					<img src={tenth} className="tenth" alt=""/>
+					<img src={seventh} className="seventh" alt=""/>
+					<img src={eighth} className="eighth" alt=""/>
+					<img src={ninth} className="ninth" alt=""/>
+					<img src={sixth} className="sixth" alt=""/>
+				</div>
+				<div className="portfolio-mobile">
+					<img src={first} className="first" alt=""/>
+					<img src={second} className="second" alt=""/>
+					<img src={third} className="third" alt=""/>
+					<img src={fourth} className="fourth" alt=""/>
+					<img src={tenth} className="tenth" alt=""/>
+					<img src={seventh} className="seventh" alt=""/>
+					<img src={eighth} className="eighth" alt=""/>
+					<img src={ninth} className="ninth" alt=""/>
+					<img src={sixth} className="sixth" alt=""/>
+				</div>
+			</Slider>
+		)
+	}
+}
 
 export default PortfolioMobile;
